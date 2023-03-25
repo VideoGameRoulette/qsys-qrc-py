c125320690954a21d55aca4e806320bc6bb4864e
Christopher D Couture <c.couture@exsi.ca>
2023-03-25 17:05:45 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,154 @@
-# qsys-qrc-py
+# Q-SYS Remote Control API
+
+This is a Python module for interfacing with the Q-SYS remote control API over a TCP/IP socket connection. The API provides a way to get and set values of controls in the Q-SYS Designer environment.
+
+## Table of Contents
+
+- [Getting started](#Getting-started)
+- [Prerequisites](#Prerequisites)
+- [Installation](#Installation)
+- [Usage](#Usage)
+
+### Functions
+- [send_command](#send_command)
+- [logon](#logon)
+- [no_op](#no_op)
+- [get_status](#get_status)
+- [get_control](#get_control)
+- [set_control](#set_control)
+- [get_component](#get_component)
+- [set_component](#set_component)
+- [get_component_controls](#get_component_controls)
+- [get_component_components](#get_component_components)
+
+## Getting started
+
+### Prerequisites
+
+- Python 3.7 or higher
+- Q-SYS Core Designer
+
+### Installation
+
+You can install this module using pip:
+
+```
+under construction
+```
+
+
+### Usage
+
+To use this module, you will need to have a Q-SYS Core Designer project open and running on your network.
+
+## send_command
+
+```py
+async def send_command(sock: socket.socket, method: str, params: Iterable[str])
+```
+
+Sends a JSON-RPC command to a Q-SYS Core device.
+
+* sock: The socket object used for communication with the Q-SYS Core device.
+* method: The name of the method to call in the JSON-RPC command.
+* params: The parameters to pass to the method.
+
+## logon
+
+```py
+async def logon(sock: socket.socket)
+```
+
+Logs in to a Q-SYS Core device.
+
+* sock: The socket object used for communication with the Q-SYS Core device.
+
+## no_op
+
+```py
+async def no_op(sock: socket.socket)
+```
+
+Sends a JSON-RPC request with no operation to the Q-SYS Core device.
+
+* sock: The socket object used for communication with the Q-SYS Core device.
+
+## get_status
+
+```py
+async def get_status(sock: socket.socket)
+```
+
+Sends a JSON-RPC request to get the status of the Q-SYS Core device.
+
+* sock: The socket object used for communication with the Q-SYS Core device.
+
+## get_control
+
+```py
+async def get_control(sock: socket.socket, name: str)
+```
+
+Sends a JSON-RPC request to get the value of a control in the Q-SYS Core device.
+
+* sock: The socket object used for communication with the Q-SYS Core device.
+* name: The name of the control to get the value of.
+
+## set_control
+
+```py
+async def set_control(sock: socket.socket, name: str, value: int, ramp: Optional[float] = None)
+```
+
+Sends a JSON-RPC request to set the value of a control in the Q-SYS Core device.
+
+* sock: The socket object used for communication with the Q-SYS Core device.
+* name: The name of the control to set the value of.
+* value: The value to set the control to.
+* ramp (optional): The time in seconds it takes to ramp the control to the new value.
+
+## get_component
+
+```py
+async def get_component(sock: socket.socket, name: str, control_name: str)
+```
+
+Sends a JSON-RPC request to get the value of a control for a specific component in the Q-SYS Core device.
+
+* sock: The socket object used for communication with the Q-SYS Core device.
+* name: The name of the component to get the control value for.
+* control_name: The name of the control to get the value of.
+
+## set_component
+
+```py
+async def set_component(sock: socket.socket, name: str, value: int)
+```
+
+Sends a JSON-RPC request to set the value of a control for a specific component in the Q-SYS Core device.
+
+* sock: The socket object used for communication with the Q-SYS Core device.
+* name: The name of the component to set the control value for.
+* value: The value to set the control to.
+
+## get_component_controls
+
+```py
+async def get_component_controls(sock: socket.socket, name: str)
+```
+
+Sends a JSON-RPC request to get all controls of a component in the Q-SYS Core device.
+
+* sock: The socket object used for communication with the Q-SYS Core device.
+* name: The name of the component to get the controls from.
+
+## get_component_components
+
+```py
+async def get_component_components(sock: socket.socket, name: str)
+```
+
+Sends a JSON-RPC request to get all child components of a component in the Q-SYS Core device.
+
+* sock: The socket object used for communication with the Q-SYS Core device.
+* name: The name of the component to get the child components from.
---
+++ main.py
@@ -0,0 +1,295 @@
+from __future__ import annotations
+from typing import Optional
+from collections.abc import Callable, Iterable
+import asyncio
+import json
+import socket
+import os
+address = user = os.environ.get('IP') or "localhost"
+port = user = os.environ.get('PORT') or 1710
+user = os.environ.get('USER') or "dev"
+pin = os.environ.get('PIN') or "1234"
+BUFFER_SIZE = 1024
+
+
+async def send_command(
+    sock: socket.socket,
+    method: str,
+    params: Iterable[str]
+):
+    """
+    https://q-syshelp.qsc.com/Content/External_Control_APIs/QRC/QRC_Commands.htm.
+    """
+    sock.send(
+        json.dumps({
+            "jsonrpc": "2.0",
+            "method": method,
+            "params": params,
+        }).encode(),
+    )
+
+
+async def logon(sock: socket.socket):
+    await send_command(
+        sock,
+        "Logon",
+        {
+            "User": user,
+            "Password": pin
+        })
+
+
+async def no_op(sock: socket.socket):
+    """
+    Sends a JSON-RPC request with no operation to the qsys remote control.
+
+    :param sock: The socket object to use for communication.
+
+    """
+    await send_command(sock, "NoOp", {})
+
+
+async def get_status(sock: socket.socket):
+    """
+    Sends a JSON-RPC request to get the status of the qsys remote control.
+
+    :param sock: The socket object to use for communication.
+
+    Expected Response:
+    {
+        "Platform": str,
+        "State": str,
+        "DesignName": str,
+        "DesignCode": str,
+        "IsRedundant": bool,
+        "IsEmulator": bool,
+        "Status":{
+          "Code": int
+          "String": str
+        }
+    }
+    """
+    await send_command(sock, "StatusGet", {})
+
+
+async def get_control(sock: socket.socket, name: str):
+    """
+    Sends a JSON-RPC request to get the value of a given control from the
+    qsys remote control.
+
+    :param sock: The socket object to use for communication.
+    :param name: The name of the control to get the value of.
+
+    Expected Response:
+    {
+        "Name": str,
+        "Value": any
+    }
+    """
+    await send_command(sock, "Control.Get", [name])
+
+
+async def set_control(
+    sock: socket.socket,
+    name: str,
+    value: int,
+    ramp: Optional[float] = None
+):
+    """
+    Sends a JSON-RPC request to set the value of a given control in the
+    qsys remote control.
+
+    :param sock: The socket object to use for communication.
+    :param name: The name of the control to set the value of, relative to
+    the component.
+    :param value: The value to set the control to. This can be a number,
+    string, or boolean.
+    :param ramp: (Optional) The ramp time used to set the control.
+    """
+    await send_command(
+        sock,
+        "Control.Set",
+        {
+            "Name": name,
+            "Value": value,
+            "Ramp": ramp
+        }
+    )
+
+
+async def get_component(sock: socket.socket, name: str, control_name: str):
+    """
+    Sends a JSON-RPC request to get the value of a given control for a
+    specified component in the qsys remote control.
+
+    :param sock: The socket object to use for communication.
+    :param name: The name of the component to get the control value for.
+    :param control_name: The name of the control to get the value of,
+    relative to the component.
+
+    Expected Response:
+    {
+        "Name": str,
+        "Controls": [
+            {
+                "Name": str,
+                "Value": float,
+                "String": str,
+                "Position": int
+            }
+        ]
+    }
+    """
+    await send_command(
+        sock,
+        "Component.Get",
+        {
+            "Name": name,
+            "Controls": [
+                {
+                    "Name": control_name
+                }
+            ]
+        }
+    )
+
+
+async def set_component(sock: socket.socket, name: str, value: int):
+    """
+    Sends a JSON-RPC request to set the value of a given control for a
+    specified component in the qsys remote control.
+
+    :param sock: The socket object to use for communication.
+    :param name: The name of the control to set the value of, relative
+    to the component.
+    :param value: The value to set the control to. This can be a number,
+    string, or boolean.
+    """
+    await send_command(
+        sock,
+        "Component.Set",
+        {
+            "Name": name,
+            "Value": value
+        }
+    )
+
+
+async def get_component_controls(sock: socket.socket, name: str):
+    """
+    Sends a JSON-RPC request to get all controls of a component from the
+    qsys remote control.
+
+    :param sock: The socket object to use for communication.
+    :param name: The name of the component to get controls from.
+
+    Expected Response:
+    {
+        "Name": str,
+        "Controls": [
+            {
+                "Name": str,
+                "Type": str,
+                "Value": any,
+                "ValueMin": float,
+                "ValueMax": float,
+                "StringMin": str,
+                "StringMax": str,
+                "String": str,
+                "Position": float,
+                "Direction": str
+            }
+        ]
+    }
+    """
+    await send_command(
+        sock,
+        "Component.GetControls",
+        {
+            "Name": name
+        }
+    )
+
+
+async def get_component_components(sock: socket.socket):
+    """
+    Sends a JSON-RPC request to get a list of components and their properties
+    from the qsys remote control.
+
+    :param sock: The socket object to use for communication.
+
+    Expected Response:
+        [
+            {
+                "Name": str,
+                "Type": str,
+                "Properties": [
+                    {
+                        "Name": str,
+                        "Value": any
+                    }
+                ]
+            }
+        ]
+    """
+    await send_command(sock, "Component.GetComponents", {})
+
+
+async def prevent_timeout(sock: socket.socket):
+    """
+    Prevents the qsys remote control from timing out by sending a NoOp
+    request every 30 seconds.
+
+    :param sock: The socket object to use for communication.
+    """
+    while True:
+        await no_op(sock)
+        await asyncio.sleep(30)
+
+
+async def listen_to_server(
+    sock: socket.socket,
+    callback: Callable[[bytes], object]
+):
+    """
+    Listens to the qsys remote control server by receiving data from the
+    socket and calling the given callback function.
+
+    :param sock: The socket object to use for communication.
+    :param callback: The callback function to call when data is received.
+    """
+    while True:
+        callback(sock.recv(BUFFER_SIZE).decode())
+
+
+def socket_message_received(encoded_message: bytes):
+    """
+    Acts as websocket orchestrator / primary controller, redirect commands
+    to the appropriate controllers/services.
+    Filter out NoOps.
+    """
+    print("Server message:", encoded_message)
+
+
+async def main():
+    # Create and listen to a sock
+    print("Initiallize Socket")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Attempting to Connect to QRC")
+    sock.connect((address, port))
+    print("Initiallizing listen_to_server func")
+    asyncio.create_task(listen_to_server(sock, socket_message_received))
+
+    # Ping-pong (ensures the socket doesn't time-out)
+    print("Initiallizing prevent_timeout func")
+    asyncio.create_task(prevent_timeout(sock))
+
+    # More commands (wait a bit before, because why not)
+    print("Sleeping for 10 Seconds")
+    await asyncio.sleep(10)
+    print("Attempting to Log In")
+    await logon(sock)
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.get_event_loop().run_forever()
